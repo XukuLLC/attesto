fefe2b58796f8d466815e300ad51d2f27a30e134
Neil Berkman <neilberkman@users.noreply.github.com>
2026-08-05 08:09:52 -0700

    Release 1.9.0 (#22)
    Version bump and changelog date for the opt-in localhost loopback matching mode and its follow-up coverage.

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Attesto.MixProject do
   alias Attesto.Test.DPoP, as: TestDPoP
   alias Attesto.Test.DPoPVerifier, as: TestDPoPVerifier
 
-  @version "1.8.1"
+  @version "1.9.0"
   @url "https://github.com/XukuLLC/attesto"
   @maintainers ["Neil Berkman"]
 
